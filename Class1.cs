28e359504734c543cdd849b52f43e38fa7459217
SakshiSuryawanshi08 <112603594+SakshiSuryawanshi08@users.noreply.github.com>
2022-09-01 17:44:35 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day_2
+{
+    class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Eligible for voting ");
+            }
+            else if (age >= 60)
+            {
+                Console.WriteLine("he is senior citizen and eligible for voting ");
+            }
+            else
+            {
+                Console.WriteLine("not eligible for voting");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace day_2
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            int a = 25;
+            int b = 20;
+            int c = 35;
+                Console.WriteLine(a +  b);
+            Console.WriteLine(c / 2);
+            Console.WriteLine(c % 2);
+           Console.WriteLine("Hello World!");
+
+            if( a > b )
+            {
+                Console.WriteLine("a is greater than b");
+            }
+            else
+            {
+                Console.WriteLine("a is smaller than b");
+            }
+
+            
+        }
+    }
+}
